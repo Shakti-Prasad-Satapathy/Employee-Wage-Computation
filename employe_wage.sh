c847a404df4751434b5cec3006141b4bf56ead35
Shakti <shaktiprasadsatapathy96@gmail.com>
2020-02-18 18:32:25 +0530

    aadded a function which look over the attendence

--- employe_wage.sh
+++ employe_wage.sh
@@ -1,6 +1,29 @@
 #!/bin/bash -x
 wage_per_hour=20
 one_day_working_hour=8
+attendence () {
+    case $randomcheck in 
+        1)
+            echo Present
+            echo Emp_type : Full_time
+            daily_wage=$((wage_per_hour * one_day_working_hour ))
+            echo daily_wage : $daily_wage
+            total_full_time_work_time=$(( total_full_time_work_time + one_day_working_hour))
+            ;;
+        2)
+                one_day_working_hour=4
+            echo Present
+            echo Emp_type : Part_time
+            daily_wage=$((wage_per_hour * one_day_working_hour ))
+            echo daily_wage : $daily_wage
+            total_part_time_work_time=$(( total_part_time_work_time + one_day_working_hour))
+            ;;
+        *)
+            echo Absent
+        echo daily_wage : 0
+            ;;
+    esac
+}
 while [[ $total_emp_work -lt 100 && $total_working_day -lt 20 ]]
 do
     for (( i = 0 ; i < 20; i++ ))
@@ -24,27 +47,8 @@ do
         echo daily_wage : 0
         fi
 
-        case $randomcheck in 
-        1)
-            echo Present
-            echo Emp_type : Full_time
-            daily_wage=$((wage_per_hour * one_day_working_hour ))
-            echo daily_wage : $daily_wage
-                total_full_time_work_time=$(( total_full_time_work_time + one_day_working_hour))
-                ;;
-        2)
-                one_day_working_hour=4
-            echo Present
-            echo Emp_type : Part_time
-            daily_wage=$((wage_per_hour * one_day_working_hour ))
-            echo daily_wage : $daily_wage
-                total_part_time_work_time=$(( total_part_time_work_time + one_day_working_hour))
-                ;;
-            *)
-                echo Absent
-            echo daily_wage : 0
-                ;;
-        esac
+        attendance
+
         monthly_wage=$(( monthly_wage + daily_wage ))
         total_working_day
     done
