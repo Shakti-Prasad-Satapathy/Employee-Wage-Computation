d5eb48922d842c1a1d1d66ed52a832bf8c0d8ba3
Shakti <shaktiprasadsatapathy96@gmail.com>
2020-02-18 18:17:59 +0530

    added Calculate Wages tilla condition of total working hours or days is reached for

--- employe_wage.sh
+++ employe_wage.sh
@@ -1,46 +1,55 @@
 #!/bin/bash -x
 wage_per_hour=20
 one_day_working_hour=8
-for (( i = 0 ; i < 20; i++ ))
-do 
-	randomcheck=$((RANDOM%3));
-	if [ $randomcheck == 1 ]
-	then
-		echo Present
-		echo Emp_type : Full_time
-		daily_wage=$((wage_per_hour * one_day_working_hour ))
-		echo daily_wage : $daily_wage
-	elif [ $randomcheck == 2 ]
-	then
-		one_day_working_hour=4
-		echo Present
-		echo Emp_type : Part_time
-	  	daily_wage=$((wage_per_hour * one_day_working_hour ))
-   	echo daily_wage : $daily_wage
-	else
-		echo Absent
-   	echo daily_wage : 0
-	fi
+while [[ $total_emp_work -lt 100 && $total_working_day -lt 20 ]]
+do
+    for (( i = 0 ; i < 20; i++ ))
+    do 
+        randomcheck=$((RANDOM%3));
+        if [ $randomcheck == 1 ]
+        then
+            echo Present
+            echo Emp_type : Full_time
+            daily_wage=$((wage_per_hour * one_day_working_hour ))
+            echo daily_wage : $daily_wage
+        elif [ $randomcheck == 2 ]
+        then
+            one_day_working_hour=4
+            echo Present
+            echo Emp_type : Part_time
+            daily_wage=$((wage_per_hour * one_day_working_hour ))
+        echo daily_wage : $daily_wage
+        else
+            echo Absent
+        echo daily_wage : 0
+        fi
 
-	case $randomcheck in 
-   	1)
-      	echo Present
-   		echo Emp_type : Full_time
-	   	daily_wage=$((wage_per_hour * one_day_working_hour ))
-   		echo daily_wage : $daily_wage
-			;;
-   	2)
-			one_day_working_hour=4
-	   	echo Present
-		   echo Emp_type : Part_time
-   		daily_wage=$((wage_per_hour * one_day_working_hour ))
-	   	echo daily_wage : $daily_wage
-			;;
-		*)
-			echo Absent
-   		echo daily_wage : 0
-			;;
-	esac
-	monthly_wage=$(( $monthly_wage + $daily_wage ))
+        case $randomcheck in 
+        1)
+            echo Present
+            echo Emp_type : Full_time
+            daily_wage=$((wage_per_hour * one_day_working_hour ))
+            echo daily_wage : $daily_wage
+                total_full_time_work_time=$(( total_full_time_work_time + one_day_working_hour))
+                ;;
+        2)
+                one_day_working_hour=4
+            echo Present
+            echo Emp_type : Part_time
+            daily_wage=$((wage_per_hour * one_day_working_hour ))
+            echo daily_wage : $daily_wage
+                total_part_time_work_time=$(( total_part_time_work_time + one_day_working_hour))
+                ;;
+            *)
+                echo Absent
+            echo daily_wage : 0
+                ;;
+        esac
+        monthly_wage=$(( monthly_wage + daily_wage ))
+        total_working_day
+    done
+	c=$((c+1))
+	total_emp_work=$(( total_full_time_work_time + total_part_time_work_time ))
 done
 echo Monthly Wage is : $monthly_wage
+echo $c
