f7862306af6ee982704e4ba70c42d1ef7f544dfb
Shakti <shaktiprasadsatapathy96@gmail.com>
2020-02-18 17:40:03 +0530

    added monthly wages calculation

--- employe_wage.sh
+++ employe_wage.sh
@@ -1,41 +1,46 @@
 #!/bin/bash -x
 wage_per_hour=20
 one_day_working_hour=8
-randomcheck=$((RANDOM%3));
-if [ $randomcheck == 1 ]
-then
-	echo Present
-	echo Emp_type : Full_time
-	daily_wage=$((wage_per_hour * one_day_working_hour ))
-	echo daily_wage : $daily_wage
-elif [ $randomcheck == 2 ]
-then
-	one_day_working_hour=4
-	echo Present
-	echo Emp_type : Part_time
-   daily_wage=$((wage_per_hour * one_day_working_hour ))
-   echo daily_wage : $daily_wage
-else
-	echo Absent
-   echo daily_wage : 0
-fi
-
-case $randomcheck in 
-   1)
-      echo Present
-   	echo Emp_type : Full_time
-   	daily_wage=$((wage_per_hour * one_day_working_hour ))
-   	echo daily_wage : $daily_wage
-		;;
-   2)
+for (( i = 0 ; i < 20; i++ ))
+do 
+	randomcheck=$((RANDOM%3));
+	if [ $randomcheck == 1 ]
+	then
+		echo Present
+		echo Emp_type : Full_time
+		daily_wage=$((wage_per_hour * one_day_working_hour ))
+		echo daily_wage : $daily_wage
+	elif [ $randomcheck == 2 ]
+	then
 		one_day_working_hour=4
-   	echo Present
-	   echo Emp_type : Part_time
-   	daily_wage=$((wage_per_hour * one_day_working_hour ))
-	   echo daily_wage : $daily_wage
-		;;
-	*)
+		echo Present
+		echo Emp_type : Part_time
+	  	daily_wage=$((wage_per_hour * one_day_working_hour ))
+   	echo daily_wage : $daily_wage
+	else
 		echo Absent
    	echo daily_wage : 0
-		;;
-esac
+	fi
+
+	case $randomcheck in 
+   	1)
+      	echo Present
+   		echo Emp_type : Full_time
+	   	daily_wage=$((wage_per_hour * one_day_working_hour ))
+   		echo daily_wage : $daily_wage
+			;;
+   	2)
+			one_day_working_hour=4
+	   	echo Present
+		   echo Emp_type : Part_time
+   		daily_wage=$((wage_per_hour * one_day_working_hour ))
+	   	echo daily_wage : $daily_wage
+			;;
+		*)
+			echo Absent
+   		echo daily_wage : 0
+			;;
+	esac
+	monthly_wage=$(( $monthly_wage + $daily_wage ))
+done
+echo Monthly Wage is : $monthly_wage
