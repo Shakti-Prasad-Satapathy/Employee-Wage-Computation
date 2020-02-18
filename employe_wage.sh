c356a1a8edb2ba48897973d2cc393a946c1a7958
Shakti <shaktiprasadsatapathy96@gmail.com>
2020-02-18 15:18:12 +0530

    finding daily wages foe an employ

--- employe_wage.sh
+++ employe_wage.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -x
-
+wage_per_hour=20
+one_day_working_hour=8
 randomcheck=$((RANDOM%2));
 if [ $randomcheck == 1 ]
 then
 	echo Present
+	daily_wage=$((wage_per_hour * one_day_working_hour ))
+	echo daily_wage : $daily_wage
 else
 	echo Absent
+	echo daily_wage : 0
 fi
