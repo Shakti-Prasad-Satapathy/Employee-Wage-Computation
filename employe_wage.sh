af6c57bbeb0c4edccd51438ccdba03f09b9a6d94
Shakti <shaktiprasadsatapathy96@gmail.com>
2020-02-18 19:07:51 +0530

    aadded an array which will store daily wages

--- employe_wage.sh
+++ employe_wage.sh
@@ -11,7 +11,7 @@ attendence () {
             total_full_time_work_time=$(( total_full_time_work_time + one_day_working_hour))
             ;;
         2)
-                one_day_working_hour=4
+            one_day_working_hour=4
             echo Present
             echo Emp_type : Part_time
             daily_wage=$((wage_per_hour * one_day_working_hour ))
@@ -20,40 +20,46 @@ attendence () {
             ;;
         *)
             echo Absent
-        echo daily_wage : 0
+            echo daily_wage : 0
             ;;
     esac
 }
+arrcount=1
+
 while [[ $total_emp_work -lt 100 && $total_working_day -lt 20 ]]
 do
-    for (( i = 0 ; i < 20; i++ ))
-    do 
-        randomcheck=$((RANDOM%3));
-        if [ $randomcheck == 1 ]
-        then
-            echo Present
-            echo Emp_type : Full_time
-            daily_wage=$((wage_per_hour * one_day_working_hour ))
-            echo daily_wage : $daily_wage
-        elif [ $randomcheck == 2 ]
-        then
-            one_day_working_hour=4
-            echo Present
-            echo Emp_type : Part_time
-            daily_wage=$((wage_per_hour * one_day_working_hour ))
+	echo xxxxxxxxxxxxxxxxxxxxxxxxx $arrcount 
+    randomcheck=$((RANDOM%3));
+    if [ $randomcheck == 1 ]
+    then
+        echo Present
+        echo Emp_type : Full_time
+        daily_wage=$((wage_per_hour * one_day_working_hour ))
         echo daily_wage : $daily_wage
-        else
-            echo Absent
-        echo daily_wage : 0
-        fi
+			
+    elif [ $randomcheck == 2 ]
+    then
+        one_day_working_hour=4
+        echo Present
+        echo Emp_type : Part_time
+        daily_wage=$((wage_per_hour * one_day_working_hour ))
+    echo daily_wage : $daily_wage
+	
+    else
+        echo Absent
+    echo daily_wage : 0
+    fi
 
-        attendance
-
-        monthly_wage=$(( monthly_wage + daily_wage ))
-        total_working_day
-    done
+    attendence
+	echo oooooooooooooooooooooooooooo $daily_wage
+	arr[arrcount]=$daily_wage
+    monthly_wage=$(( monthly_wage + daily_wage ))
+    total_working_day
 	c=$((c+1))
 	total_emp_work=$(( total_full_time_work_time + total_part_time_work_time ))
+	arrcount=$(( arrcount + 1 ))
 done
 echo Monthly Wage is : $monthly_wage
 echo $c
+echo daily wage is : ${arr[@]}
+
